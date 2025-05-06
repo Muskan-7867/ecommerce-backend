d89188bf7fe86251b000089ff8b95b7604c25a94
Muskan-7867 <muskanloach984@gmail.com>
2025-05-06 18:03:19 +0530

    update model

--- src/models/order.model.js
+++ src/models/order.model.js
@@ -1,35 +1,53 @@
 import mongoose from "mongoose";
-
 const orderSchema = new mongoose.Schema({
   quantity: {
     type: Number,
     required: true
   },
 
-  user: {
+  client: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User"
   },
 
-  product: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Product",
-    required: true
-  },
+  orderItems: [{
+    price: { type: Number, required: true },
+    quantity: { type: Number, required: true },
+    product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true }
+  }],
 
   address: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Address",
     required: true
   },
-  status:{
+  
+  totalPrice : {
+    type: Number,
+    required: true
+  },
+
+  totalQuantity : {
+    type: Number,
+    required: true
+  },
+
+  payment: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Payment"
+  },
+
+  isPaid: {
+    type: Boolean,
+    default: false
+  },
+
+  status: {
     type: String,
     enum: ["pending", "shipped", "delivered", "cancelled"],
     default: "pending"
-  }
-});
+  },
+  
+}, { timestamps: true});
 
 export const Order = mongoose.model("Order", orderSchema);
-
-
-

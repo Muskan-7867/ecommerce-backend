f6a70bbdd069edb1d701b180af0212d091c9feef
Muskan-7867 <muskanloach984@gmail.com>
2025-05-17 17:14:22 +0530

    add routes

--- src/router/route.js
+++ src/router/route.js
@@ -1,11 +1,14 @@
 
+import { adminRouter } from "./admin.router.js";
 import { productRouter } from "./product.router.js";
 import { UserRouter } from "./user.router.js";
 import { Router } from "express";
 const router = Router();
 
 router.use("/api/v1/user", UserRouter);
-router.use("/api/v2/product", productRouter);
+router.use("/api/v1/product", productRouter);
+router.use("/api/v1/admin", adminRouter);
+
 
 
 export default router;

9c3ff1a7ebd830e12d45b10bb5cd60bc6946fe36
Muskan-7867 <muskanloach984@gmail.com>
2025-07-04 14:01:08 +0530

    update

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const port = 3000;
 
 connectDB();
 const corsOptions = {
-  origin: ["https://omeg-bazaar-client.vercel.app", "http://localhost:5173"],
+  origin: ["https://omeg-bazaar-client.vercel.app", "http://localhost:5173", "https://omegbazaar.com"],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true,
   optionSuccessStatus: 200,
